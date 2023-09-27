d90df86dc55387627129a74497af1f4539bcb7b6
WorriorBeast <128549074+WorriorBeast@users.noreply.github.com>
2023-09-27 15:15:46 -0700

    Allow user to customize grid size

--- javascript.js
+++ javascript.js
@@ -1,13 +1,46 @@
 const gridContainer = document.querySelector('.grid-container');
 
-for (i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.toggle('individual-grid');
-    gridContainer.appendChild(div);
+function defaultGrid() {
+    for (i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('default-grid');
+        gridContainer.appendChild(div);
+    }
+    const divs = document.querySelectorAll('.default-grid');
+
+    divs.forEach(div => div.addEventListener('mousemove', () => {
+    div.classList.add('div-hidden');
+    }));
+}
+defaultGrid();
+
+function customGrid(sizeInput) {
+    let gridSize = Math.pow(sizeInput,2);
+    let gridPixelSize = 640 / sizeInput;
+
+    for (i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.classList.toggle('individual-grid');
+        div.setAttribute('style', `height: ${gridPixelSize}px; width: ${gridPixelSize}px;`)
+        gridContainer.appendChild(div);
+    }
+    const divs = document.querySelectorAll('.individual-grid');
+
+    divs.forEach(div => div.addEventListener('mousemove', () => {
+        div.classList.add('div-hidden');
+    }));
 }
 
-const divs = document.querySelectorAll('.individual-grid');
+function button() {
+    let sizeInput = Number(prompt('What size grid do you want?\nMaximum 100 x 100\nMinimum 1 x 1'));
 
-divs.forEach(div => div.addEventListener('mousemove', () => {
-    div.classList.add('div-hidden');
-}));
+    if (!Number.isInteger(sizeInput)) alert('Please enter a whole number');
+
+    if (sizeInput <= 0 || sizeInput > 100) alert('Please enter a number from 1 to 100');
+
+    customGrid(sizeInput);
+}
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', button);
